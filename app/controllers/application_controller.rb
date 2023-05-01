4b8f47409c7b764426acabd9cb9b24d153b7fdc7
Juanpa8830 <jp3_4@hotmail.com>
2023-05-01 15:39:03 -0500

    Update applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_in) do |u|
+        u.permit(:email, :encrypted_password)
+      end
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:name, :email, :password, :password_confirmation)
+      end
+    end
 end
