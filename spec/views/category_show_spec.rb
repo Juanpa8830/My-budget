c7800620124bee531fd7231305dd4a3fbcea213b
Juanpa8830 <jp3_4@hotmail.com>
2023-05-04 15:58:21 -0500

    Add tests for views

---
+++ spec/views/category_show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'categories/show', type: :feature do
+  include Warden::Test::Helpers
+
+  before do
+    @user = User.create(name: 'Juan Pablo', email: 'juanpa@gmail.com', password: '123456',
+                        password_confirmation: '123456')
+    @user.skip_confirmation!
+    @user.save
+    login_as(@user, scope: :user)
+    @category = Category.create(name: 'Trips', icon: 'dog.png', user: @user)
+    @movement = Movement.create(name: 'Tickets', amount: 90.0, user: @user)
+    visit category_path(@category)
+  end
+
+  it 'displays App name' do
+    expect(page).to have_content("TRANSACTIONS")
+  end
+
+  it 'displays category name' do
+    expect(page).to have_content('Trips')
+  end
+
+  it 'displays category name' do
+    expect(page).to have_content(@category.name)
+  end
+end
